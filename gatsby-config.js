42121714de0aa47b53d374639e3a72661bd999f6
Nvi mai <nvimai2703@gmail.com>
2020-02-04 22:58:31 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/static/images`,
         name: `images`,
       },
